2c147cc4250296f4e60a208fe0304e664db77de6
ismailhg <ismailhgndgd@gmail.com>
2026-02-23 23:42:37 +0300

    Project setup complete

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black text-white h-screen flex items-center justify-center">
+      Tailwind çalısıyor
+    </div>
+  );
+}
+
+export default App;
